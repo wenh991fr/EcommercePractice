a05847321d99a19c641f406a0621803f25a70487
wenh991fr <arcangel_wen@hotmail.com>
2026-03-05 12:20:00 -0600

    Se añade el README

--- EcommerceApi/obj/Debug/net9.0/EcommerceApi.AssemblyInfo.cs
+++ EcommerceApi/obj/Debug/net9.0/EcommerceApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EcommerceApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc7deb12c4b0bfeda1fc39e921de903ac37b239e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b995757461b71e4237ce7591ccd04606a0fa7402")]
 [assembly: System.Reflection.AssemblyProductAttribute("EcommerceApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EcommerceApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
